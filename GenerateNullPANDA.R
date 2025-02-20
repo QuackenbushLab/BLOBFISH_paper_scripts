f8b4e602e84e7a9ec996d475dc782c526921049d
Tara Eicher <taraeicher@yahoo.com>
2025-02-20 09:34:15 -0500

    Generalized the null model generation script file paths

--- GenerateNullPANDA.R
+++ GenerateNullPANDA.R
@@ -1,5 +1,12 @@
-setwd("/home/ubuntu/netZooR")
-roxygen2::roxygenize()
+# Load the network zoo.
+library("netZooR")
 
-null <- GenerateNullPANDADistribution(ppiFile = "/home/ubuntu/ppi.txt", motifFile = "/home/ubuntu/motif.txt")
-saveRDS(null, "/home/ubuntu/nullPANDA.RDS")
+# Add PPI file and motif file paths and the directory where you wish to save the
+# null model.
+ppiFilePath <- NULL
+motifFilePath <- NULL
+nullFilePath <- NULL
+
+# Generate and save null model.
+null <- GenerateNullPANDADistribution(ppiFile = ppiFilePath, motifFile = motifFilePath)
+saveRDS(null, nullFilePath)
